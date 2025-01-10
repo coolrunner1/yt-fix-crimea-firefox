252abd714eecf5dcdbb021185b00b76da7addca8
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2025-01-10 19:37:19 +0300

    Wrote an extension

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # yt-fix-crimea-firefox
+This extension fixes an youtube issue (GenerateIT 403) which causes playback to stop at 1 minute mark. Extension accomplishes this by swapping youtube player with an embedded youtube player. Just push the red "Click Me!" button.
---
+++ yt-fix.js
@@ -0,0 +1,32 @@
+const swap = () => {
+	document.querySelector("ytd-player").remove();
+	const playerContainer = document.querySelector("#player");
+	playerContainer.innerHTML = '';
+	const iframe = document.createElement("iframe");
+	iframe.src="https://www.youtube.com/embed/"+window.location.search.substring(3).split("&")[0]+"?autoplay=1";
+	iframe.allow='autoplay';
+	iframe.style.height='100%';
+	iframe.style.minHeight="720px";
+	iframe.style.width='100%';
+	playerContainer.append(iframe);
+}
+
+if (window.location.pathname === "/watch") {
+	const button = document.createElement('button');
+	button.innerText = 'Click Me!';
+	button.style.position = 'fixed';
+	button.style.top = '10px';
+	button.style.right = '10px';
+	button.style.zIndex = '1';
+	button.style.padding = '10px';
+	button.style.marginTop = '50px';
+	button.style.backgroundColor = '#ff0000';
+	button.style.color = '#ffffff';
+	button.style.border = 'none';
+	button.style.borderRadius = '5px';
+	button.style.cursor = 'pointer';
+
+	button.addEventListener('click', () => swap());
+
+	document.body.appendChild(button);
+}
